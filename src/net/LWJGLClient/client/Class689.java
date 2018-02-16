5391c0daa41e2ac7aede70f2485a871a21a3bb88
LWJGLClientOpenSource <36459422+LWJGLClientOpenSource@users.noreply.github.com>
2018-02-16 00:22:55 -0600

    Update Class689.java

--- src/net/LWJGLClient/client/Class689.java
+++ src/net/LWJGLClient/client/Class689.java
@@ -3,7 +3,6 @@ package net.LWJGLClient.client;
 import net.LWJGLClient.client.model.e.Class112;
 
 final class Class689 implements Runnable {
-   // $FF: synthetic field
    private final Class112 Field2891;
 
    Class689(Class112 var1) {
